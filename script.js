451ae805f68a5309eb61032eadefc18426b1574d
eneibaur <107266929+eneibaur@users.noreply.github.com>
2023-01-19 08:43:35 -0800

    add linter to project

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function books(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    function info() {
-      const string = 'this.title + " by " this.author + "," + this.pages + " pages, " + this.read';
-      return string;
-    }
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  function info() {
+    const string = 'this.title + " by " this.author + "," + this.pages + " pages, " + this.read';
+    return string;
   }
-  
-  const theHobbit = Books('The Hobbit', 'J.R.R. Tolkien', 295, 'Not read yet');
-  
-  console.log(theHobbit.info);
+}
+
+const theHobbit = Books('The Hobbit', 'J.R.R. Tolkien', 295, 'Not read yet');
+
+console.log(theHobbit.info);
